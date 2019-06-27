80d2cc5b144f0eb3491eda03e9e78238c00aa706
msanaullahsahar <52190123+msanaullahsahar@users.noreply.github.com>
2019-06-27 21:48:16 +1000

    Create clearPics.py

---
+++ clearPics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import glob
+import os
+directory='/home/pi/motion'
+os.chdir(directory)
+fileExt = ('*.avi', '*.jpg')
+files_grabbed = []
+for files in fileExt:
+	files_grabbed.extend(glob.glob(files))
+
+for filename in files_grabbed:
+	os.unlink(filename)
