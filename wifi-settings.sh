17ebf950a337a9e56064917b65a5a9965acfae72
Sahar <52190123+msanaullahsahar@users.noreply.github.com>
2019-09-09 07:25:04 +1000

    Update wifi-settings.sh

--- wifi-settings.sh
+++ wifi-settings.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get install whiptail -y
 sudo apt-get install wget -y
 echo -ne '\007'
-# Ask permission to set wifi
+# Ask permission to set WiFi
 whiptail --title "Permission" --yesno "WiFi-Settings Script. Do you want to continue (yes/no)?" 10 60
 exitstatus=$?
 if [ $exitstatus = 1 ]; then
@@ -11,23 +11,17 @@ exit 1 || return 1
 fi
 echo -e "\e[31;43m***** WiFi-Settings begins *****\e[0m"
 # Enter SSID
-while [[ -z "$SSID" ]]
-do
-  SSID=$(whiptail --inputbox "What is your WiFi name?" 8 78 --title "WiFi SSID" 3>&1 1>&2 2>&3)
-  exitstatus=$?
-	if [ $exitstatus = 1 ];then
+SSID=$(whiptail --inputbox "What is your WiFi name?" 8 78 --title "WiFi SSID" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 1 ]; then
 	exit 1 || return 1
-	fi
-done
+fi
 # Enter Password
-while [[ -z "$PSWD" ]]
-do
-  PSWD=$(whiptail --inputbox "What is your WiFi name?" 8 78 --title "WiFi Password" 3>&1 1>&2 2>&3)
-  exitstatus=$?
-	if [ $exitstatus = 1 ];then
+PSWD=$(whiptail --inputbox "What is your WiFi name?" 8 78 --title "WiFi Password" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 1 ]; then
 	exit 1 || return 1
-	fi
-done
+fi
 # Download WiFi Config file
 sudo rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo wget https://raw.githubusercontent.com/msanaullahsahar/nestv2/master/wpa_supplicant.conf
@@ -38,5 +32,5 @@ sudo sed -i -e 's/myPassword/$PSWD/g' /etc/wpa_supplicant/wpa_supplicant.conf
 if (whiptail --title "Reboot Permission" --yesno "Do you want to reboot now (y/n)?" 10 60) then
 sudo reboot now
 else
-echo "Please reboot manually ...."
+echo "Please reboot otherwise Thingsboard platform will not start ...."
 fi
