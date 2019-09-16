6bd14b31c8098aef6caf6dda0e9054ab72288b3a
Sahar <msahar@msn.com>
2019-09-16 14:26:59 +1000

    Update WiFi settings

--- wifi-settings.sh
+++ wifi-settings.sh
@@ -9,15 +9,21 @@ exitstatus=$?
 if [ $exitstatus = 1 ]; then
 exit 1 || return 1
 fi
-echo -e "\e[31;43m***** WiFi-Settings begins *****\e[0m"
-# Enter SSID
+echo -e "\e[30;48;5;82m***** WiFi-Settings begins *****\e[0m"
+# Enter WiFi SSID
 SSID=$(whiptail --inputbox "What is your WiFi name?" 8 78 --title "WiFi SSID" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 1 ]; then
 	exit 1 || return 1
 fi
-# Enter Password
+# Enter WiFi Password
 PSWD=$(whiptail --inputbox "Please supply your WiFi Password?" 8 78 --title "WiFi Password" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 1 ]; then
+	exit 1 || return 1
+fi
+# Enter your country code
+CCODE=$(whiptail --inputbox "Please enter two letters code of your country? For example, The two letters code for Australia is AU" 8 78 --title "WiFi Country Code" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 1 ]; then
 	exit 1 || return 1
@@ -26,11 +32,12 @@ fi
 sudo rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo wget https://raw.githubusercontent.com/msanaullahsahar/nestv2/master/wpa_supplicant.conf
 sudo mv wpa_supplicant.conf /etc/wpa_supplicant/
+sudo sed -i -e "s/myCountry/$CCODE/g" /etc/wpa_supplicant/wpa_supplicant.conf
 sudo sed -i -e "s/mySSID/$SSID/g" /etc/wpa_supplicant/wpa_supplicant.conf
 sudo sed -i -e "s/myPassword/$PSWD/g" /etc/wpa_supplicant/wpa_supplicant.conf
 # Reboot System
 if (whiptail --title "Reboot Permission" --yesno "Do you want to reboot now (y/n)?" 10 60) then
 sudo reboot now
 else
-echo "Please reboot manually ...."
+echo -e "\e[30;48;5;82m***** Please reboot manually .... *****\e[0m"
 fi
