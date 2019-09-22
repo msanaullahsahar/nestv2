7356fbcbf0b602e611ded0a698906f1d0bf8aee4
msanaullahsahar <hello@sahar.pk>
2019-09-22 11:12:15 +1000

    Update Thingsboard Installation
    Update for Install Thingsboard Process.

--- installTB.sh
+++ installTB.sh
@@ -14,17 +14,35 @@ sudo apt-get install unzip -y
 sudo apt-get install curl -y
 sudo apt-get install default-jdk -y
 echo -ne '\007'
-echo -e "\e[30;48;5;82m*** Downloading ThingsBoard IoT Platform ***\e[0m"
-wget https://github.com/thingsboard/thingsboard/releases/download/v2.2/thingsboard-2.2.deb 2>&1 |\
+clear
+echo -e "\e[31;43m*** Downloading Latest ThingsBoard Package ***\e[0m"
+URL=$(curl -s https://api.github.com/repos/thingsboard/thingsboard/releases/latest | grep browser_download_url.*deb | cut -d '"' -f 4)
+sleep 5
+wget "$URL" 2>&1 |\
 stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
 dialog --gauge "Downloading Thingsboard Platform. The downloading process may take some minutes depending on the speed of your internet." 10 100
-clear
+thingsboard="$(find . -name "*.deb")"
 echo -ne '\007'
-echo -e "\e[30;48;5;82m*** Installing ThingsBoard ***\e[0m"
-sudo dpkg -i thingsboard-2.2.deb
-sudo /usr/share/thingsboard/bin/install/install.sh
+echo -e "\e[30;48;5;82m*** Making ThingsBoard Platform Ready for Installation***\e[0m"
+sudo dpkg -i $thingsboard
+echo -e "\e[30;48;5;82m*** Installing PostgreSQL ***\e[0m"
+sudo apt-get install -y postgresql postgresql-contrib
+sudo service postgresql restart
+su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'post24984';\""
+sudo -u postgres psql -c 'create database thingsboard;'
 echo -e "\e[30;48;5;82m*** Restrict ThingsBoard to 512MB of memory usage ***\e[0m"
 echo 'export JAVA_OPTS="$JAVA_OPTS -Dplatform=rpi -Xms512M -Xmx512M"' >> /etc/thingsboard/conf/thingsboard.conf
+echo -e "\e[30;48;5;82m*** Configuring database for Thingsboard ***\e[0m"
+echo '# DB Configuration' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export DATABASE_ENTITIES_TYPE=sql' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export DATABASE_TS_TYPE=sql' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export SPRING_JPA_DATABASE_PLATFORM=org.hibernate.dialect.PostgreSQLDialect' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export SPRING_DRIVER_CLASS_NAME=org.postgresql.Driver' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5432/thingsboard' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export SPRING_DATASOURCE_USERNAME=postgres' >> /etc/thingsboard/conf/thingsboard.conf
+echo 'export SPRING_DATASOURCE_PASSWORD=post24984' >> /etc/thingsboard/conf/thingsboard.conf
+echo -e "\e[30;48;5;82m*** Installing ThingsBoard Platform ***\e[0m"
+sudo /usr/share/thingsboard/bin/install/install.sh
 echo -e "\e[30;48;5;82m***** Starting Thingsboard as a Service *****\e[0m"
 sudo service thingsboard start
 echo -e "\e[30;48;5;82m*** Finding IP address of the Thingsboard IOT Platform ***\e[0m"
